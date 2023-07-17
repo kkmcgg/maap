35d65ad15970f4e6fe28a3ac1c879118ada049eb
Kevin Mcguigan <kevin.mcguigan@nscc.ca>
2023-07-17 15:32:28 -0300

    secrets

--- public/script.js
+++ public/script.js
@@ -1,9 +1,14 @@
-// Obtain access token after successful sign-in
-const authResponse = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse();
-const accessToken = authResponse.access_token;
+// // Obtain access token after successful sign-in
+// const authResponse = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse();
+// const accessToken = authResponse.access_token;
 
-// Authenticate Earth Engine API with the access token
-ee.data.authenticateViaOauth(accessToken);
+// // Authenticate Earth Engine API with the access token
+// ee.data.authenticateViaOauth(accessToken);
+
+const apiKey = process.env.API_KEY;
+
+// Authenticate Earth Engine API with the API key
+ee.data.authenticateViaAPIKey(apiKey);
 
 // Make an Earth Engine API request
 ee.Image('COPERNICUS/S2_SR/20210701T045621_20210701T050249_T44PVS').getInfo(function(imageInfo) {
